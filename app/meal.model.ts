f97d04659169af40598a7f0cb1dec3c64041900e
Brad <bradcopenhaver@gmail.com>
2017-01-20 15:14:28 -0800

    add type to day in Meal constructor and clean up code

--- app/meal.model.ts
+++ app/meal.model.ts
@@ -1,6 +1,5 @@
 export class Meal {
   public hours = new Date().getHours();
   public minutes = new Date().getMinutes();
-  constructor(public foods: string, public calories: number, public notes: string, public dateString: string = new Date().toDateString(), public day = new Date().getDay()) {
-  }
+  constructor(public foods: string, public calories: number, public notes: string, public dateString: string = new Date().toDateString(), public day: number = new Date().getDay()) {}
 }
